dc3e29e340893795b54584238dc07dc553d2cff1
alvit007 <alvitokurnia@gmail.com>
2023-12-04 20:58:01 +0700

    add footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <div className="h-[3.5rem] bg-white">
+      <div className="flex justify-center items-center h-full px-4">
+          <div className="text-primary text-xl font-medium">
+          Copyright © Bigbang And Dinosaur  
+          </div>
+      </div>
+    </div>
   )
 }
 
