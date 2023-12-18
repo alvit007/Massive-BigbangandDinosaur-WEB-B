d260e99a68358a8490a102f7a359fb3a683a6aad
alvit007 <alvitokurnia@gmail.com>
2023-12-18 22:00:36 +0700

    mahasiswatambah masi error

--- src/pages/Mahasiswa/MahasiswaTambah.jsx
+++ src/pages/Mahasiswa/MahasiswaTambah.jsx
@@ -1,67 +1,68 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
 import Layout from "../Layout";
+import { useParams, useNavigate } from "react-router-dom";
+import axios from "axios";
 
 function MahasiswaTambah() {
   const [npm, setNpm] = useState("");
-
-  const [token, setToken] = useState("");
-  const [formData, setFormData] = useState({
-    nama_mahasiswa: "",
-    npm: "",
-    jk: "",
-    alamat: "",
-    foto: null,
-    status: "",
-    notlp: "",
-    email: "",
-    password: "",
-    id_kelas: "",
-    nama_kelas: "",
-  });
-
-  useEffect(() => {
-    const fetchToken = async () => {
-      try {
-        const tokenResponse = await axios.post(`/api/v1/tambahmahasiswa`, {});
-        setToken(tokenResponse.data.token);
-      } catch (error) {
-        console.error("Gagal mengambil token:", error);
-      }
-    };
-
-    fetchToken();
-  }, []);
-
-  const fetchData = async () => {
-    try {
-      const response = await axios.get("/api/v1/tambahmahasiswa", {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      });
-
-      // Lakukan sesuatu dengan data yang diterima
-      console.log(response.data);
-    } catch (error) {
-      console.error("Gagal mengambil data:", error);
-    }
-  };
-
-  useEffect(() => {
-    if (token) {
-      fetchData();
-    }
-  }, [token]);
+  const [nama_mahasiswa, setNamaMahasiswa] = useState("");
+  const [jk, setJenisKelamin] = useState("");
+  const [alamat, setAlamat] = useState("");
+  const [status, setStatus] = useState("");
+  const [notlp, setNoTelepon] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [id_kelas, setIdKelas] = useState("");
+  const [nama_kelas, setNamaKelas] = useState("");
+  const navigate = useNavigate();
 
   const handleInputChange = (e) => {
     const { name, value } = e.target;
-    setFormData({ ...formData, [name]: value });
+    switch (name) {
+      case "nama_mahasiswa":
+        setNamaMahasiswa(value);
+        break;
+      case "npm":
+        setNpm(value);
+        break;
+      case "jk":
+        setJenisKelamin(value);
+        break;
+      case "alamat":
+        setAlamat(value);
+        break;
+      case "status":
+        setStatus(value);
+        break;
+      case "noTelepon":
+        setNoTelepon(value);
+        break;
+      case "email":
+        setEmail(value);
+        break;
+      case "password":
+        setPassword(value);
+        break;
+      case "kelas":
+        setIdKelas(value);
+        break;
+      case "namaKelas":
+        setNamaKelas(value);
+        break;
+      default:
+        break;
+    }
   };
 
   const handleFileChange = (e) => {
     const { name, files } = e.target;
-    setFormData({ ...formData, [name]: files[0] });
+    switch (name) {
+      case "foto":
+        // Handle file change logic if needed
+        break;
+      default:
+        break;
+    }
   };
 
   const handleSubmit = async (e) => {
@@ -70,19 +71,23 @@ function MahasiswaTambah() {
     const apiUrl = '/api/v1/tambahmahasiswa';
 
     const formDataToSend = new FormData();
-    for (const key in formData) {
-      formDataToSend.append(key, formData[key]);
-    }
+    formDataToSend.append("nama_mahasiswa", nama_mahasiswa);
+    formDataToSend.append("npm", npm);
+    formDataToSend.append("jk", jk);
+    formDataToSend.append("alamat", alamat);
+    formDataToSend.append("status", status);
+    formDataToSend.append("notlp", notlp);
+    formDataToSend.append("email", email);
+    formDataToSend.append("password", password);
+    formDataToSend.append("id_kelas", id_kelas);
+    formDataToSend.append("nama_kelas", nama_kelas);
 
     try {
-      const response = await axios.post(apiUrl, formDataToSend, {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      });
+      const response = await axios.post(apiUrl, formDataToSend);
 
       if (response.status === 200) {
         console.log('Mahasiswa berhasil ditambahkan');
+        navigate('/path-setelah-submit'); // Ganti dengan path yang sesuai
       } else {
         console.error('Gagal menambahkan Mahasiswa');
       }
@@ -114,7 +119,7 @@ function MahasiswaTambah() {
               <input
                 type="text"
                 name="nama_mahasiswa"
-                value={formData.nama_mahasiswa}
+                value={nama_mahasiswa}
                 onChange={handleInputChange}
                 className="mt-1 p-2 w-4/5 border rounded-md pl-7 text-primary bg-background"
                 placeholder="Masukkan Nama Mahasiswa"
@@ -128,7 +133,7 @@ function MahasiswaTambah() {
               <input
                 type="text"
                 name="npm"
-                value={formData.npm}
+                value={npm}
                 onChange={handleInputChange}
                 className="mt-1 p-2 w-4/5 border rounded-md pl-7 text-primary bg-background"
                 placeholder="Masukkan NPM"
@@ -142,7 +147,7 @@ function MahasiswaTambah() {
               <input
                 type="text"
                 name="jk"
-                value={formData.jk}
+                value={jk}
                 onChange={handleInputChange}
                 className="mt-1 p-2 w-4/5 border rounded-md pl-7 text-primary bg-background"
                 placeholder="Masukkan Jenis Kelamin"
@@ -156,7 +161,7 @@ function MahasiswaTambah() {
               <input
                 type="text"
                 name="alamat"
-                value={formData.alamat}
+                value={alamat}
                 onChange={handleInputChange}
                 className="mt-1 p-2 w-4/5 border rounded-md pl-7 text-primary bg-background"
                 placeholder="Masukkan Alamat"
@@ -182,7 +187,7 @@ function MahasiswaTambah() {
               <input
                 type="text"
                 name="status"
-                value={formData.status}
+                value={status}
                 onChange={handleInputChange}
                 className="mt-1 p-2 w-4/5 border rounded-md pl-7 text-primary bg-background"
                 placeholder="Masukkan Status"
@@ -196,7 +201,7 @@ function MahasiswaTambah() {
               <input
                 type="text"
                 name="noTelepon"
-                value={formData.notlp}
+                value={notlp}
                 onChange={handleInputChange}
                 className="mt-1 p-2 w-4/5 border rounded-md pl-7 text-primary bg-background"
                 placeholder="Masukkan No Telepon"
@@ -210,7 +215,7 @@ function MahasiswaTambah() {
               <input
                 type="text"
                 name="email"
-                value={formData.email}
+                value={email}
                 onChange={handleInputChange}
                 className="mt-1 p-2 w-4/5 border rounded-md pl-7 text-primary bg-background"
                 placeholder="Masukkan Email"
@@ -224,7 +229,7 @@ function MahasiswaTambah() {
               <input
                 type="password"
                 name="password"
-                value={formData.password}
+                value={password}
                 onChange={handleInputChange}
                 className="mt-1 p-2 w-4/5 border rounded-md pl-7 text-primary bg-background"
                 placeholder="Masukkan Password"
@@ -238,7 +243,7 @@ function MahasiswaTambah() {
               <input
                 type="text"
                 name="kelas"
-                value={formData.id_kelas}
+                value={id_kelas}
                 onChange={handleInputChange}
                 className="mt-1 p-2 w-4/5 border rounded-md pl-7 text-primary bg-background"
                 placeholder="Masukkan Kelas"
@@ -252,7 +257,7 @@ function MahasiswaTambah() {
               <input
                 type="text"
                 name="namaKelas"
-                value={formData.nama_kelas}
+                value={nama_kelas}
                 onChange={handleInputChange}
                 className="mt-1 p-2 w-4/5 border rounded-md pl-7 text-primary bg-background"
                 placeholder="Masukkan Nama Kelas"
